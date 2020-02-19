c4cf18108f2439cf2826d5c3bdc02be1d7fa7b81
poikilos <7557867+poikilos@users.noreply.github.com>
2020-02-19 17:23:11 -0500

    Finish documenting extract.

--- anewcommit/__init__.py
+++ anewcommit/__init__.py
@@ -4,13 +4,20 @@
 def extract(src_file, new_parent_dir, auto_sub=True,
             auto_sub_name=None):
     """
+    Extract any known archive file type to a specified directory.
+
     Sequential arguments:
     src_file -- Extract this archive file.
     new_parent_dir -- Place the extracted files into this directory
         (after temp directory).
 
     Keyword arguments:
-    auto_sub -- Automatically create a subdirectory
+    auto_sub -- Automatically create a subdirectory only if there is
+        more than one item directly under the root of the archive. If
+        False, extract as-is to new_parent_dir (even if that results in
+        a subdirectory that is the name of the original directory).
+    auto_sub_name -- If auto_sub is true, rename the extracted or
+        created directory to the value of this string.
     """
     raise NotImplementedError("There is nothing implemented here yet.")
 
